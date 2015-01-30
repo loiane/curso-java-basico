179f7cba719fd3ee0ed61ae7396576a593c292a8
loiane <loianeg@gmail.com>
2015-01-30 13:57:04 -0200

    added aula 13 - labs - eclipse

--- eclipse/curso-java-basico/src/com/loiane/cursojava/aula13/labs/Exer03.java
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula13/labs/Exer03.java
@@ -1,27 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package com.loiane.cursojava.aula13.labs;
 
 import java.util.Scanner;
 
+/**
+ *
+ * @author loiane
+ */
 public class Exer03 {
-
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		//primeira solucao
-		/*System.out.println("Entre com dois números inteiros:");
-		int num1 = scan.nextInt();
-		int num2 = scan.nextInt();
-		int resultado = num1 + num2;
-		System.out.println("A soma dos números é: " + resultado);*/
-		
-		//segunda solucao
-		System.out.println("Entre com um número inteiro:");
-		int num1 = scan.nextInt();
-		System.out.println("Entre com outro número inteiro:");
-		int num2 = scan.nextInt();
-		int resultado = num1 + num2;
-		System.out.println("A soma dos números é: " + resultado);
-	}
-
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Entre com o primeiro número:");
+        int num1 = scan.nextInt();
+        
+        System.out.println("Entre com o segundo número:");
+        int num2 = scan.nextInt();
+        
+        int resultado = num1 + num2;
+        System.out.println("A soma dos números é: " + resultado);
+    }
 }
