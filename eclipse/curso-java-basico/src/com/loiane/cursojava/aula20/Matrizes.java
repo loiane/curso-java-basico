7e9d1fba00478063993b2fe23b6e1337589f7b7c
Loiane Groner <loianeg@gmail.com>
2015-04-06 11:08:52 -0300

    added aula 20 - matrizes

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula20/Matrizes.java
@@ -0,0 +1,25 @@
+package com.loiane.cursojava.aula20;
+
+public class Matrizes {
+
+	public static void main(String[] args) {
+		
+		double[][] notasAlunos = new double[30][4];
+
+		notasAlunos[0][0] = 10;
+		notasAlunos[0][1] = 7;
+		notasAlunos[0][2] = 9;
+		notasAlunos[0][3] = 9.5;
+		
+		notasAlunos[1][0] = 9;
+		notasAlunos[1][1] = 8;
+		notasAlunos[1][2] = 7;
+		notasAlunos[1][3] = 9;
+		
+		notasAlunos[2][0] = 8;
+		notasAlunos[2][1] = 9;
+		notasAlunos[2][2] = 10;
+		notasAlunos[2][3] = 7;
+	}
+
+}
