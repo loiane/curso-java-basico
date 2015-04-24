da334b0e7f8d9684e2ee29e24c0e5ee8607c84e5
Loiane Groner <loianeg@gmail.com>
2015-04-24 14:04:46 -0300

    add aula 22 - eclipse

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula22/DebugEclipse.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+		
+		int[] notas = new int[3];
+		
+		for (int i=0; i<4; i++){
+			notas[i] = i;
+		}
+
+	}
+
+}
