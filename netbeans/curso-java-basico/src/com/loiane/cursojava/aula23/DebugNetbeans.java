230015bfb03bec7f3e4b0816f2937e4bd54911e9
Loiane Groner <loianeg@gmail.com>
2015-04-24 14:26:29 -0300

    added aula 23 - debug netbeans

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula23/DebugNetbeans.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.loiane.cursojava.aula23;
+
+/**
+ *
+ * @author loiane
+ */
+public class DebugNetbeans {
+
+    public static void main(String[] args) {
+
+        int[] notas = new int[3];
+
+        for (int i = 0; i < 4; i++) {
+            notas[i] = i;
+        }
+
+    }
+}
