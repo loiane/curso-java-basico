a443136585a2c4ff5aedab8701541ace5cb3818b
Loiane Groner <loianeg@gmail.com>
2015-05-20 14:38:23 -0300

    add aula 31 - modificares de acesso public, private e default

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula31alt/TesteCarroAlt.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula31alt;
+
+import com.loiane.cursojava.aula31.Carro;
+
+public class TesteCarroAlt {
+
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		carro.marca = "Fiat";
+
+	}
+
+}
