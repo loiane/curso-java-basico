46dffa6a93223c7a293b79828cb363e7d072811d
Loiane Groner <loianeg@gmail.com>
2015-05-20 14:39:17 -0300

    add aula 30 - palavra chave this

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula30/TesteCarro.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula30;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro("Fiat", "Ducato");
+
+		System.out.println(van.numPassageiros);
+	}
+
+}
