e8ce8091e3efd3184b4a684f62f06ee5ec190e83
Loiane Groner <loianeg@gmail.com>
2015-09-21 11:09:45 -0300

    add aula 32 - encapsulamento - métodos get e set

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula32/Carro.java
@@ -0,0 +1,42 @@
+package com.loiane.cursojava.aula32;
+
+public class Carro {
+
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capCombustivel;
+	private double consumoCombustivel;
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getNumPassageiros() {
+		return numPassageiros;
+	}
+	public void setNumPassageiros(int numPassageiros) {
+		this.numPassageiros = numPassageiros;
+	}
+	public double getCapCombustivel() {
+		return capCombustivel;
+	}
+	public void setCapCombustivel(double capCombustivel) {
+		this.capCombustivel = capCombustivel;
+	}
+	public double getConsumoCombustivel() {
+		return consumoCombustivel;
+	}
+	public void setConsumoCombustivel(double consumoCombustivel) {
+		this.consumoCombustivel = consumoCombustivel;
+	}
+}
---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula32/TesteCarro.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		
+		van.setMarca("Fiat");
+		
+		System.out.println(van.getMarca());
+
+	}
+
+}
