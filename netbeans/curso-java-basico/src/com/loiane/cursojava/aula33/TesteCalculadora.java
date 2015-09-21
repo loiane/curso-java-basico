c250086e67267f941df09abb0476a3c74b185b05
Loiane Groner <loianeg@gmail.com>
2015-09-21 11:09:57 -0300

    add aula 33 - sobrecarga de métodos e construtores

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula33/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula33;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		MinhaCalculadora calc = new MinhaCalculadora();
+		
+		calc.soma(1, 2);
+		calc.soma(1.0, 2.0);
+	}
+
+}
