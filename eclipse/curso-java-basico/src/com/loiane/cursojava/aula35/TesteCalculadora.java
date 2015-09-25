9aaaa00bbc118c882e698115bf2c0a766a46d654
Loiane Groner <loianeg@gmail.com>
2015-09-25 10:40:12 -0300

    added aula 35

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula35/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula35;
+
+public class TesteCalculadora {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.fatorial(5));
+		System.out.println(Calculadora.fatorialRecursivo(5));
+
+	}
+
+}
