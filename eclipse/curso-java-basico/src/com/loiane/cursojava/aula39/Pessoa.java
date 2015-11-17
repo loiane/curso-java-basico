0632603b2fc211b3b49c1aecc5a2bdf8c141c918
Loiane Groner <loianeg@gmail.com>
2015-11-17 18:28:07 -0200

    aula 39

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula39/Pessoa.java
@@ -0,0 +1,59 @@
+package com.loiane.cursojava.aula39;
+
+public class Pessoa {
+
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String cpf;
+	private String telefoneCelular;
+	
+	protected String nomeVisibilidade;
+	
+	public Pessoa() {
+		nomeVisibilidade = "sdfsdfsfsdf";
+	}
+	
+	public Pessoa(String nome, String endereco, String telefone) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+
+	public void setNomeVisibilidade(String nomeVisibilidade) {
+		this.nomeVisibilidade = nomeVisibilidade;
+	}
+	
+	
+}
