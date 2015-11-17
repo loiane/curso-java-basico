23f82845e298bd28643f19901dd442c8d531d996
Loiane Groner <loianeg@gmail.com>
2015-11-17 18:04:29 -0200

    aula 37

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula37/Professor.java
@@ -0,0 +1,25 @@
+package com.loiane.cursojava.aula37;
+
+public class Professor extends Pessoa {
+
+	private double salario;
+	private String nomeCurso;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double calcularSalarioLiquido(){
+		return 0;
+	}
+		
+}
---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula37/Teste.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula37;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		/*Aluno aluno = new Aluno();
+		
+		Professor professor = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		aluno.setNome("Maria");
+		*/
+		//professor.setTelefoneCelular(telefoneCelular);
+		
+		Pessoa aluno = new Aluno();
+		//aluno.get
+		
+		Pessoa professor = new Professor();
+		//professor.getSala
+	}
+
+}
