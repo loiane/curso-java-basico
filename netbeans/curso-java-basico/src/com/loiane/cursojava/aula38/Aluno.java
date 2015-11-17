82b286c22e30fdb0afff1bf305cf8915821029eb
Loiane Groner <loianeg@gmail.com>
2015-11-17 18:29:45 -0200

    aula 38 - netbeans

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula38/Aluno.java
@@ -0,0 +1,43 @@
+package com.loiane.cursojava.aula38;
+
+public class Aluno extends Pessoa {
+
+	private String curso;
+	private double[] notas;
+	
+	public Aluno() {
+		super();
+	}
+	
+	public Aluno(String nome, String endereco, String telefone, String curso) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia(){
+		return 0;
+	}
+	
+	public boolean verificarAprovado(){
+		return true;
+	}
+	
+	public void metodoQualquer(){
+		super.setCpf("345345345354");
+		
+		this.setCpf("34534534");
+	}
+}
