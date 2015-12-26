bd38ea86e2c518fbbac3020101754c5c914d4ae4
Loiane Groner <loianeg@gmail.com>
2015-12-26 12:03:23 -0200

    aula 42

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula42/Contantes.java
@@ -0,0 +1,9 @@
+package com.loiane.cursojava.aula42;
+
+public final class Contantes {
+
+	public static final String URL_BLOG = "http://loiane.com";
+	public static final String CURSO_COMPLETO = "http://loiane.training";
+	
+	//private String urlBlog;
+}
---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula42/Teste.java
@@ -0,0 +1,23 @@
+package com.loiane.cursojava.aula42;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa = new Pessoa();
+		//Pessoa aluno = new Aluno();
+		//Pessoa professor = new Professor();
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+
+		//exemplo 02
+		System.out.println(Contantes.URL_BLOG);
+		//Contantes.URL_BLOG = "sdjfskjdhfsdf";
+		System.out.println(Contantes.URL_BLOG);
+		
+		final int TOTAL = 1; 
+		//total = total = 1;
+		System.out.println(TOTAL);
+	}
+
+}
