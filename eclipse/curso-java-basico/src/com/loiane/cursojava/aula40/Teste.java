01a94beff6e7de33a0697219cfb1717135fe2273
Loiane Groner <loianeg@gmail.com>
2015-12-26 11:30:27 -0200

    aula 40

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula40/Teste.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula40;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+
+		pessoa.setEndereco("Rua 1, num 1");
+		aluno.setEndereco("Rua 2, num 2");
+		professor.setEndereco("Rua 3, num 3");
+		 
+		System.out.println(pessoa.obterEtiquetaEndereco());
+		System.out.println(aluno.obterEtiquetaEndereco());
+		System.out.println(professor.obterEtiquetaEndereco());
+	}
+
+}
