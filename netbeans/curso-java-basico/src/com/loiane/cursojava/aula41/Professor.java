0d966e6d0fc80d335ae717e685f5faa8db823e30
Loiane Groner <loianeg@gmail.com>
2016-01-03 15:25:56 -0200

    aula 41 - netbeans

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula41/Professor.java
@@ -0,0 +1,39 @@
+package com.loiane.cursojava.aula41;
+
+public class Professor extends Pessoa {
+
+	private double salario;
+	private String nomeCurso;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	
+	public double calcularSalarioLiquido(){
+		return 0;
+	}
+		
+	public String obterEtiquetaEndereco(){
+
+		String s = "Endereço do Professor: ";
+		s += super.getEndereco();
+		
+		return s;
+	}
+	@Override
+	public void imprimirEtiquetaEndereco() {
+		
+		System.out.println("Imprimindo endereço do Professor");
+		System.out.println(this.obterEtiquetaEndereco());
+		
+	}
+}
---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula41/Teste.java
@@ -0,0 +1,22 @@
+package com.loiane.cursojava.aula41;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		//Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+
+		//pessoa.setEndereco("Rua 1, num 1");
+		aluno.setEndereco("Rua 2, num 2");
+		professor.setEndereco("Rua 3, num 3");
+
+		//System.out.println(aluno.obterEtiquetaEndereco());
+		//System.out.println(professor.obterEtiquetaEndereco());
+		
+		aluno.imprimirEtiquetaEndereco();
+		professor.imprimirEtiquetaEndereco();
+	}
+
+}
