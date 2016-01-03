d325b8a944e314a4fcf5e9c21c81125a4f0f838e
Loiane Groner <loianeg@gmail.com>
2016-01-03 15:26:13 -0200

    aula 42 - netbeans

---
+++ netbeans/curso-java-basico/src/com/loiane/cursojava/aula42/Pessoa.java
@@ -0,0 +1,53 @@
+package com.loiane.cursojava.aula42;
+
+public final class Pessoa {
+
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String cpf;
+	
+	private String telefoneCelular;
+	
+	public Pessoa() {}
+	public Pessoa(String nome, String endereco, String telefone) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+	
+	//public abstract String obterEtiquetaEndereco();
+	
+	//public abstract void imprimirEtiquetaEndereco();
+}
