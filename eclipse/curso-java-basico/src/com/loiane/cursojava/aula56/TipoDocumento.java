acb45ea0e6d14ba4e89fcdd84eacca17b7fca9b5
Loiane Groner <loianeg@gmail.com>
2016-04-07 19:51:12 -0300

    aula 56 - enum

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula56/TipoDocumento.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula56;
+
+public enum TipoDocumento {
+
+	CPF {
+		@Override
+		public String geraNumeroTeste() {
+			return GeraCpfCnpj.cpf();
+		}
+	}, CNPJ {
+		@Override
+		public String geraNumeroTeste() {
+			return GeraCpfCnpj.cnpj();
+		}
+	};
+	
+	public abstract String geraNumeroTeste();
+}
