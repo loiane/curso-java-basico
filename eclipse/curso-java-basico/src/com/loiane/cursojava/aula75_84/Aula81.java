d58c0b3a841a58d0c2a00de381e683b4cd9ebf73
loiane <loianeg@gmail.com>
2016-08-08 18:47:06 -0300

    aula 81

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula75_84/Aula81.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula75_84;
+
+public class Aula81 {
+
+	public static void main(String[] args) {
+		
+		String teste = "Teste";
+		
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);
+		
+		//if (teste.toLowerCase().equals(teste.toLowerCase()))
+	}
+
+}
