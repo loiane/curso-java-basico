7d18abe63dde2549363faec045349eb7c51d1d4b
loiane <loianeg@gmail.com>
2016-08-12 16:40:51 -0300

    aula 84

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula75_84/Aula84.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula75_84;
+
+import java.util.StringTokenizer;
+
+public class Aula84 {
+
+	public static void main(String[] args) {
+		
+		String doArquivo = "1;Antônio;30;";
+		
+		StringTokenizer st = new StringTokenizer(doArquivo, ";");
+		
+		while (st.hasMoreTokens()){
+			System.out.println(st.nextToken());
+		}
+
+	}
+
+}
