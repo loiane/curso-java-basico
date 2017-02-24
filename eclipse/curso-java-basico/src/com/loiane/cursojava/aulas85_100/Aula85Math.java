97a90545a334526cb03ae359450b6686a4071dc5
loiane <loianeg@gmail.com>
2017-02-24 15:49:32 -0300

    aula 85: Math

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aulas85_100/Aula85Math.java
@@ -0,0 +1,31 @@
+package com.loiane.cursojava.aulas85_100;
+
+public class Aula85Math {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.pow(2, 3));
+		
+		System.out.println(Math.round(4.7));
+		
+		System.out.println(Math.round(4.4));
+		
+		System.out.println(Math.ceil(4.4));
+		
+		System.out.println(Math.ceil(4.7));
+		
+		System.out.println(Math.floor(4.4));
+		
+		System.out.println(Math.floor(4.7));
+		
+		System.out.println(Math.round(Math.random() * 100));
+		
+		System.out.println(Math.sin(Math.toRadians(30)));
+		
+		System.out.println(Math.cos(Math.toRadians(1)));
+		
+		System.out.println(Math.tan(Math.toRadians(45)));
+
+	}
+
+}
